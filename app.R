dd5f5c1dd6a32c3ff8caf462fcd2332ae88dc414
cwai097 <95458565+cwai097@users.noreply.github.com>
2022-10-17 21:15:19 -0600

    table added

--- app.R
+++ app.R
@@ -22,7 +22,7 @@ ui <- fluidPage(theme = shinytheme("flatly"),
                     ),
                     conditionalPanel(
                       condition = "input.dropDownMenu == 'Descriptive Statistics'",
-                      textAreaInput("descriptiveStat", "Sample", value=NULL, placeholder = "Enter values seperated by a comma with decimals as points", rows = 6), #make wider and taller
+                      textAreaInput("descriptiveStat", "Sample", value= "2.14,2.09,2.65,3.56,5.55,5.00,5.55,3.09,6.79", placeholder = "Enter values seperated by a comma with decimals as points", rows = 6), #make wider and taller
                       #checkboxGroupInput(inputId = "checkBoxDescpStats", "Select", selected = c("Mean", "Mode","Median"),choices = c("Mean","Mode","Median","Standard Deviation","Interquartile Range","Box Plot")), #default mean, mode, median, must select at least once
                       actionButton(inputId = "goDescpStats", "Calculate") #blue
                     ),
@@ -225,9 +225,12 @@ ui <- fluidPage(theme = shinytheme("flatly"),
                   ),
                   
                   mainPanel(
-                    conditionalPanel(
-                      condition = "input.dropDownMenu == 'Descriptive Statistics'",
-                      uiOutput("resultsDescpStats")
+                    div( id="despStats",
+                      conditionalPanel(
+                        condition = "input.dropDownMenu == 'Descriptive Statistics'",
+                        tableOutput("table")
+                    )
+                    
                     )
                   )
                 )
@@ -244,46 +247,56 @@ server <- function(input, output) {
     as.numeric(split)
   }
   # 
-  # # Modes <- function(x) {
-  # #   ux <- unique(x)
-  # #   tab <- tabulate(match(x, ux))
-  # #   ux[tab == max(tab)]
-  # # }
+  Modes <- function(x) {
+    ux <- unique(x)
+    tab <- tabulate(match(x, ux))
+    ux[tab == max(tab)]
+  }
   # 
   # # Mode <- function(x) {
   # #   a <- table(x)
   # #   as.numeric(names(a)[a == max(a)])
   # # }
   # 
-  output$resultsDescpStats <- renderUI({
+  
+  
+  observeEvent(input$goDescpStats, {
     dat <- createNumLst(input$descriptiveStat)
     if(anyNA(dat) | length(dat)<2){
       "Invalid input or not enough observations"
     } else{
-      withMathJax(
-        paste0("\\(\\bar{x}=\\)", mean(dat)),
-        #paste0("Mode = ", Modes(dat)),
-        br(),
-        paste0("Median (Q2) =", median(dat)),
-        br(),
-        #paste("Five Number Summary:"),
-        #br(),
-        #paste0(min(dat), " ", quantile(dat,0.25)," ", median(dat), " ", quantile(dat, 0.75)," ", max(dat)),
-        #br(),
-        paste0("Q1: ", quantile(dat,0.25)),
-        br(),
-        paste0("Q3: ", quantile(dat,0.75)),
-        br(),
-        paste0("IQR:",IQR(dat)),
-        br(),
-        paste0("Standard Deviation: ",sd(dat)),
-        br(),
-        paste0("Variance: ",var(dat)),
-        br(),
-        paste0("Range",range(dat)[2]-range(dat)[1]),
-        br()
+      dat <- createNumLst(input$descriptiveStat)
+      print(dat)
+      values <- reactiveValues()
+      values$df <- data.frame(Variable = character(), Value = character())
+      output$table <- renderTable(values$df)
+      row1 <- data.frame(Variable = "Mean", Value = paste0(mean(dat)))
+      row2 <- data.frame(Variable = "Median", Value = paste0(median(dat)))
+      values$df <- rbind(row1, row2)
+      # withMathJax(
+      #   paste0("\\(\\bar{x}=\\)", mean(dat)),
+      #   #paste0("Mode = ", Modes(dat)),
+      #   br(),
+      #   paste0("Median (Q2) =", median(dat)),
+      #   br(),
+      #   #paste("Five Number Summary:"),
+      #   #br(),
+      #   #paste0(min(dat), " ", quantile(dat,0.25)," ", median(dat), " ", quantile(dat, 0.75)," ", max(dat)),
+      #   #br(),
+      #   paste0("Q1: ", quantile(dat,0.25)),
+      #   br(),
+      #   paste0("Q3: ", quantile(dat,0.75)),
+      #   br(),
+      #   paste0("IQR:",IQR(dat)),
+      #   br(),
+      #   paste0("Standard Deviation: ",sd(dat)),
+      #   br(),
+      #   paste0("Variance: ",var(dat)),
+      #   br(),
+      #   paste0("Range",range(dat)[2]-range(dat)[1]),
+      #   br()
         #paste0("Boxplot", boxplot(c(dat)))
-      )
+      
     }
   })
 
