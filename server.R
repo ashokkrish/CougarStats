1f0293c138ae9ded3b2ba5ef9d75478a25d5e23c
Michael Myer <m-myer@live.com>
2023-11-10 19:38:53 -0700

    SI: updated one mean HT output structure

--- server.R
+++ server.R
@@ -4318,7 +4318,7 @@ server <- function(session, input, output) {
       sprintf("For a \\( %s \\)%% Confidence Interval: ",
               ConfLvl()*100),
       br(),
-      sprintf("\\( \\phantom{CII} \\alpha = 1 - %s = %s \\)",
+      sprintf("\\( \\alpha = 1 - %s = %s \\)",
               ConfLvl(),
               1 - ConfLvl()),
       br())
@@ -4340,7 +4340,7 @@ server <- function(session, input, output) {
     
     if(OneMeanSigma() == "Known"){
       cvOutput <- tagList(
-                          sprintf("\\( \\phantom{CII} z_{\\alpha/2} = z_{%s/2} = z_{%s} = %s \\)",
+                          sprintf("\\( z_{\\alpha/2} = z_{%s/2} = z_{%s} = %s \\)",
                                   1 - ConfLvl(),
                                   (1 - ConfLvl()) / 2,
                                   oneMeanData["Z Critical"]),
@@ -4351,9 +4351,9 @@ server <- function(session, input, output) {
       df <- oneMeanData["Sample Size"] - 1
       
       cvOutput <- tagList(
-                          sprintf("\\( \\phantom{CII} df = n - 1 \\)"),
+                          sprintf("\\( df = n - 1 \\)"),
                           br(),
-                          sprintf("\\( \\phantom{CII} t_{\\alpha/2, \\, df} = t_{%s/2, \\, %s} = t_{%s, \\, %s} = %s \\)",
+                          sprintf("\\( t_{\\alpha/2, \\, df} = t_{%s/2, \\, %s} = t_{%s, \\, %s} = %s \\)",
                                   1 - ConfLvl(),
                                   df,
                                   (1 - ConfLvl()) / 2,
@@ -4378,34 +4378,39 @@ server <- function(session, input, output) {
       }
       
       givenOutput <- tagList(
+        br(),
         sprintf("Given:"),
         br(),
-        sprintf("\\( \\phantom{CII} n = %s \\)",
+        sprintf("\\( n = %s \\)",
                 oneMeanData['Sample Size']),
         br(),
-        sprintf("\\( \\phantom{CII} \\bar{x} = %s \\)",
+        sprintf("\\( \\bar{x} = %s \\)",
                 oneMeanData['Sample Mean']),
         br(),
-        sprintf("\\( \\phantom{CII} %s = %s \\)",
+        sprintf("\\( %s = %s \\)",
                 sd,
                 oneMeanData[3]),
         br(),
         br(),
+        br()
       )
       
     } else {
       
       if(OneMeanSigma() == 'Known') {
         givenOutput <- tagList(
+          br(),
           sprintf("Given:"),
           br(),
-          sprintf("\\( \\phantom{CII} \\sigma = %s \\)",
+          sprintf("\\( \\sigma = %s \\)",
                   oneMeanData[3]),
           br(),
           br(),
+          br()
         )
       } else {
         givenOutput <- br()
+
       }
       
     }
@@ -4421,67 +4426,65 @@ server <- function(session, input, output) {
       sd <- "s"
       testStat <- "t_{\\alpha/2, \\, df}"
     }
+
+    formulaOutput <- tagList(
+      sprintf("\\( \\displaystyle CI = \\bar{x} \\pm \\left( %s \\dfrac{%s}{\\sqrt{n}} \\right) \\)",
+              testStat,
+              sd),
+      br()
+    )
     
-    if(input$dataAvailability == 'Summarized Data') {
+    if(input$dataAvailability != "Summarized Data") {
+      formulaOutput <- tagAppendChild(formulaOutput, printOneMeanWhere(oneMeanData))
+    } else {
+      formulaOutput <- tagAppendChildren(formulaOutput, br(), br())
+    }
+    
+    return(formulaOutput)
+  }
+  
+  printOneMeanWhere <- function(oneMeanData) {
+
+    if(OneMeanSigma() == 'Known') {
       formulaOutput <- tagList(
+        sprintf("where"),
         br(),
-        sprintf("\\( \\displaystyle CI = \\bar{x} \\pm \\left( %s \\dfrac{%s}{\\sqrt{n}} \\right) \\)",
-                testStat,
-                sd),
+        sprintf("\\( \\phantom{CII} n = %s \\; , \\)",
+                oneMeanData["Sample Size"]),
+        sprintf("\\( \\phantom{CII} \\bar{x} = \\dfrac{\\sum x}{n} = \\dfrac{%s}{%s} = %s \\)",
+                OneMeanTotaledData()[1],
+                oneMeanData["Sample Size"],
+                oneMeanData["Sample Mean"]),
         br(),
         br(),
         br()
       )
     } else {
-      
-      if(OneMeanSigma() == 'Known') {
-        formulaOutput <- tagList(
-          br(),
-          sprintf("\\( \\displaystyle CI = \\bar{x} \\pm \\left( %s \\dfrac{%s}{\\sqrt{n}} \\right) \\)",
-                  testStat,
-                  sd),
-          br(),
-          br(),
-          sprintf("where"),
-          br(),
-          sprintf("\\( \\phantom{CII} n = %s \\; , \\)",
-                  oneMeanData["Sample Size"]),
-          sprintf("\\( \\phantom{CII} \\bar{x} = \\dfrac{\\sum x}{n} = \\dfrac{%s}{%s} = %s \\)",
-                  OneMeanTotaledData(),
-                  oneMeanData["Sample Size"],
-                  oneMeanData["Sample Mean"]),
-          br(),
-          br(),
-          br()
-        )
-      } else {
-        formulaOutput <- tagList(
-          br(),
-          sprintf("\\( \\displaystyle CI = \\bar{x} \\pm \\left( %s \\dfrac{%s}{\\sqrt{n}} \\right) \\)",
-                  testStat,
-                  sd),
-          br(),
-          br(),
-          sprintf("where"),
-          br(),
-          sprintf("\\( \\phantom{CII} n = %s \\; , \\)",
-                  oneMeanData["Sample Size"]),
-          sprintf("\\( \\phantom{CII} \\bar{x} = \\dfrac{\\sum x}{n} = \\dfrac{%s}{%s} = %s \\; , \\)",
-                  OneMeanTotaledData(),
-                  oneMeanData["Sample Size"],
-                  oneMeanData["Sample Mean"]),
-          sprintf("\\( \\phantom{CII} s  = \\sqrt{ \\dfrac{\\sum x^{2} - \\dfrac{(\\sum x)^{2}}{n} }{n - 1} } = %s \\)", 
-                  oneMeanData[3]),
-          br(),
-          br(),
-          br()
-        )
-        
-      } 
-
-    }
+      formulaOutput <- tagList(
+        sprintf("where"),
+        br(),
+        sprintf("\\( \\phantom{CII} n = %s \\; , \\)",
+                oneMeanData["Sample Size"]),
+        sprintf("\\( \\phantom{CII} \\bar{x} = \\dfrac{\\sum x}{n} = \\dfrac{%s}{%s} = %s \\; , \\)",
+                OneMeanTotaledData()[1],
+                oneMeanData["Sample Size"],
+                oneMeanData["Sample Mean"]),
+        br(),
+        sprintf("and"),
+        br(),
+        sprintf("\\( \\phantom{CII} s  = \\sqrt{ \\dfrac{\\sum x^{2} - \\dfrac{(\\sum x)^{2}}{n} }{n - 1} } \\)"),
+        sprintf("\\( = \\sqrt{ \\dfrac{%s - \\dfrac{(%s)^{2}}{%s} }{%s - 1} } = %s \\)",
+                OneMeanTotaledData()[2],
+                OneMeanTotaledData()[1],
+                oneMeanData["Sample Size"],
+                oneMeanData["Sample Size"],
+                oneMeanData[3]),
+        br(),
+        br(),
+        br()
+      )
+    } 
     
-    return(formulaOutput)
   }
   
   printOneMeanCICalc <- function() {
@@ -4537,6 +4540,214 @@ server <- function(session, input, output) {
     
     return(intrpOutput)
   }
+  
+  printOneMeanHT <- function() {
+    
+    oneMeanData <- GetOneMeanHT()
+    intrpInfo <- OneMeanHypInfo()
+    
+    if(OneMeanSigma() == 'Known') {
+      sdSymbol <- "\\sigma"
+      testStat <- "z"
+    } else {
+      sdSymbol <- "s"
+      testStat <- "t"
+    }
+    
+    if(oneMeanData[7] > SigLvl())
+    {
+      pvalSymbol <- "\\gt"
+      suffEvidence <- "do not provide"
+      reject <- "do not reject"
+      region <- "acceptance"
+    }
+    else
+    {
+      pvalSymbol <- "\\leq"
+      suffEvidence <- "provide"
+      reject <- "reject"
+      region <- "rejection"
+    }
+    
+    oneMeanHTOutput <- tagList(
+      withMathJax(),
+      sprintf("\\( H_{0}: \n \\mu %s %s\\)",
+              intrpInfo$nullHyp,
+              input$hypMean),
+      br(),
+      sprintf("\\( H_{a}: \\mu %s %s\\)",
+              intrpInfo$altHyp,
+              input$hypMean),
+      br(),
+      br(),
+      sprintf("\\( \\alpha = %s \\)",
+              SigLvl()),
+      br(),
+      br())
+    
+    givenOutput <- printOneMeanGiven()
+    formulaOutput <- printOneMeanHTFormula(sdSymbol, testStat)
+    pvalOutput <- printOneMeanHTPVal(pvalSymbol, reject)
+    cvOutput <- printOneMeanHTCV(testStat, reject, region)
+    conclusionOutput <- printOneMeanHTConclusion(suffEvidence)
+    
+    tagAppendChildren(oneMeanHTOutput, givenOutput, formulaOutput, pvalOutput, cvOutput, conclusionOutput)
+  }
+  
+  printOneMeanHTFormula <- function(sdSymbol, testStat) {
+    oneMeanData <- GetOneMeanHT()
+    
+    formulaOutput <- tagList(
+      sprintf("\\(%s = \\dfrac{\\bar{x} - \\mu_{0}}{ \\dfrac{%s}{\\sqrt{n}} } \\)",
+            testStat,
+            sdSymbol),
+      br()
+    )
+    
+    if(input$dataAvailability != "Summarized Data") {
+      formulaOutput <- tagAppendChild(formulaOutput, printOneMeanWhere(oneMeanData)) 
+    } else {
+      formulaOutput <- tagAppendChildren(formulaOutput, br(), br())
+    }
+    
+    calcOutput <- tagList(
+      sprintf("\\(%s =  \\dfrac{%s - %s}{ \\dfrac{%s}{\\sqrt{%s}} }\\)",
+              testStat,
+              oneMeanData[2],
+              input$hypMean,
+              oneMeanData[3],
+              oneMeanData[1]),
+      sprintf("\\( = \\dfrac{%0.4f}{%s} \\)",
+              oneMeanData[2] - input$hypMean,
+              oneMeanData["Std Error"]),
+      br(),
+      br(),
+      sprintf("\\(\\phantom{%s} = %0.4f\\)",
+              testStat,
+              oneMeanData[6]),
+      br(),
+      br(),
+      br()
+    )
+    
+    formulaOutput <- tagAppendChild(formulaOutput, calcOutput)
+    return(formulaOutput)
+  }
+  
+  printOneMeanHTCV <- function(testStat, reject, region) {
+    oneMeanData <- GetOneMeanHT()
+    
+    if(OneMeanHypInfo()$alternative == "two.sided")
+    {
+      if(testStat == 'z') {
+        critVal <- paste("\\pm", oneMeanData["Z Critical"])
+      } else {
+        critVal <- paste("\\pm", oneMeanData["T Critical"])
+      }
+      
+    }
+    else
+    {
+      if(testStat == 'z') {
+        critVal <- paste(oneMeanData["Z Critical"])
+      } else {
+        critVal <- paste(oneMeanData["T Critical"])
+      }
+    }
+    
+    if(testStat == 'z') {
+      
+      cvOutput <- tagList(
+        p(tags$b("Using Critical Value Method:")),
+        sprintf("Critical Value(s) \\( = %s z_{%s} = %s z_{%s} = %s \\)",
+                OneMeanHypInfo()$critSign,
+                OneMeanHypInfo()$critAlph,
+                OneMeanHypInfo()$critSign,
+                OneMeanHypInfo()$alphaVal,
+                critVal),
+        br(),
+      )
+      
+    } else {
+      
+      cvOutput <- tagList(
+        p(tags$b("Using Critical Value Method:")),
+        sprintf("\\( df = n - 1 = %s \\)",
+                oneMeanData["Sample Size"] - 1),
+        br(),
+        sprintf("Critical Value(s) \\( = %s t_{%s, \\, df} = %s t_{%s, \\, %s} = %s \\)",
+                OneMeanHypInfo()$critSign,
+                OneMeanHypInfo()$critAlph,
+                OneMeanHypInfo()$critSign,
+                OneMeanHypInfo()$alphaVal,
+                oneMeanData["Sample Size"] - 1,
+                critVal),
+        br()
+      )
+    }
+    
+    cvEnd <- tagList(
+      sprintf("Since the test statistic \\( (%s)\\) falls within the %s region, %s \\( H_{0}\\).",
+              testStat,
+              region,
+              reject),
+      br(),
+      br(),
+      plotOutput('oneMeanHTPlot', width = "75%", height = "300px"),
+      br()
+    )
+    
+    cvOutput <- tagAppendChild(cvOutput, cvEnd)
+    return(cvOutput)
+  }
+  
+  printOneMeanHTPVal <- function(pvalSymbol, reject) {
+    oneMeanData <- GetOneMeanHT()
+    
+    if(oneMeanData[7] < 0.0001)
+    {
+      pValue <- "\\lt 0.0001"
+    }
+    else
+    {
+      pValue <- paste(oneMeanData[7])
+    }
+    
+    pvalOutput <- tagList(
+      p(tags$b("Using P-Value Method:")),
+      sprintf("\\(P = %s\\)",
+              pValue),
+      br(),
+      sprintf("Since \\( P %s %0.2f \\), %s \\( H_{0}\\).",
+              pvalSymbol,
+              SigLvl(),
+              reject),
+      br(),
+      br(),
+      br(),
+    )
+    
+    return(pvalOutput)
+  }
+  
+  printOneMeanHTConclusion <- function(suffEvidence) {
+    
+    conclusion <- tagList(
+      p(tags$b("Conclusion:")),
+      p(
+        sprintf("At the %1.0f%% significance level, the data %s sufficient evidence to reject the null hypothesis \\( (H_{0}) \\) that the population 
+                              mean \\( (\\mu) \\) \\( %s %g\\).",
+                SigLvl()*100,
+                suffEvidence,
+                OneMeanHypInfo()$nullHyp,
+                input$hypMean),
+        br(),
+      )
+    )
+    
+    return(conclusion)
+  }
+  
   
   GetDepMeansData <- function() {
     req(si_iv$is_valid())
@@ -4906,7 +5117,8 @@ server <- function(session, input, output) {
       dat <- 0
     }
     
-    return(sum(dat))
+    totaled <- list(sum(dat), sum(dat^2))
+    return(totaled)
   })
   
   OneMeanHypInfo <- reactive({
@@ -5125,6 +5337,29 @@ server <- function(session, input, output) {
     }
     
     return(oneMeanCI)
+  })
+  
+  GetOneMeanHT <- reactive({
+    
+    if(OneMeanSigma() == "Known"){
+      
+      if(input$dataAvailability == 'Summarized Data'){
+        oneMeanHT <- OneMeanZTestSumm()
+      } else {
+        oneMeanHT <- OneMeanZTestRaw()
+      }
+      
+    } else {
+      
+      if(input$dataAvailability == 'Summarized Data'){
+        oneMeanHT <- OneMeanTTestSumm()
+      } else {
+        oneMeanHT <- OneMeanTTestRaw()
+      }
+      
+    }
+    
+    return(oneMeanHT)
   })
   
   #### Independent Sample Means reactives ----
@@ -5776,175 +6011,9 @@ server <- function(session, input, output) {
   
   ##### HT ----
   output$oneMeanHT <- renderUI({
-    withMathJax()
     
-    if(input$dataAvailability == 'Summarized Data'){
-      
-      if(input$sigmaKnown == 'Known'){
-        oneMeanData <- OneMeanZTestSumm()
-        sdSymbol <- "\\sigma"
-        testStat <- "z"
-      }
-      else if(input$sigmaKnown == 'Unknown'){
-        oneMeanData <- OneMeanTTestSumm()
-        sdSymbol <- "s"
-        testStat <- "t"
-      } 
-    } else if(input$dataAvailability == 'Enter Raw Data'){
-      
-      if(input$sigmaKnownRaw == 'rawKnown'){
-        oneMeanData <- OneMeanZTestRaw()
-        sdSymbol <- "\\sigma"
-        testStat <- "z"
-      }
-      else if(input$sigmaKnownRaw == 'rawUnknown'){
-        oneMeanData <- OneMeanTTestRaw()
-        sdSymbol <- "s"
-        testStat <- "t"
-      } 
-    } else if(input$dataAvailability == 'Upload Data'){
-      
-      if(input$sigmaKnownUpload == 'Known'){
-        
-        oneMeanData <- OneMeanZTestRaw()
-        sdSymbol <- "\\sigma"
-        testStat <- "z"
-        critVal <- oneMeanData["Z Critical"]
-        
-      } else if(input$sigmaKnownUpload == 'Unknown'){
-        
-        oneMeanData <- OneMeanTTestRaw()
-        sdSymbol <- "s"
-        testStat <- "t"
-        critVal <- oneMeanData["T Critical"]
-      } 
-    }
+    printOneMeanHT()
     
-    intrpInfo <- OneMeanHypInfo()
-    
-    if(oneMeanData[7] < 0.0001)
-    {
-      pValue <- "\\lt 0.0001"
-    }
-    else
-    {
-      pValue <- paste(oneMeanData[7])
-    }
-    
-    if(oneMeanData[7] > SigLvl())
-    {
-      pvalSymbol <- "\\gt"
-      suffEvidence <- "do not provide"
-      reject <- "do not reject"
-      region <- "acceptance"
-    }
-    else
-    {
-      pvalSymbol <- "\\leq"
-      suffEvidence <- "provide"
-      reject <- "reject"
-      region <- "rejection"
-    }
-    
-    if(intrpInfo$alternative == "two.sided")
-    {
-      if(testStat == 'z') {
-        critVal <- paste("\\pm", oneMeanData["Z Critical"])
-      } else {
-        critVal <- paste("\\pm", oneMeanData["T Critical"])
-      }
-      
-    }
-    else
-    {
-      if(testStat == 'z') {
-        critVal <- paste(oneMeanData["Z Critical"])
-      } else {
-        critVal <- paste(oneMeanData["T Critical"])
-      }
-      
-    }
-    
-    tagList(
-      
-      p(
-        withMathJax(
-          #h4(tags$u("Performing the Hypothesis Test:")),
-          #br(),
-          sprintf("\\( H_{0}: \n \\mu %s %s\\)",
-                  intrpInfo$nullHyp,
-                  input$hypMean),
-          br(),
-          sprintf("\\( H_{a}: \\mu %s %s\\)",
-                  intrpInfo$altHyp,
-                  input$hypMean),
-          br(),
-          br(),
-          sprintf("\\( \\alpha = %s \\)",
-                  SigLvl()),
-          br(),
-          br(),
-          br(),
-          sprintf("\\(%s = \\dfrac{\\bar{x} - \\mu_{0}}{ \\dfrac{%s}{\\sqrt{n}} } = \\dfrac{%s - %s}{ \\dfrac{%s}{\\sqrt{%s}} }\\)",
-                  testStat,
-                  sdSymbol,
-                  oneMeanData[2],
-                  input$hypMean,
-                  oneMeanData[3],
-                  oneMeanData[1]),
-          br(),
-          br(),
-          sprintf("\\(\\phantom{%s} = %0.4f\\)",
-                  testStat,
-                  oneMeanData[6]),
-          br(),
-          br(),
-          br(),
-          p(tags$b("Using P-Value Method:")),
-          sprintf("\\(P = %s\\)",
-                  pValue),
-          br(),
-          sprintf("Since \\( P %s %0.2f \\), %s \\( H_{0}\\).",
-                  pvalSymbol,
-                  SigLvl(),
-                  reject),
-          br(),
-          br(),
-          br(),
-          p(tags$b("Using Critical Value Method:")),
-          sprintf("Critical Value(s) \\( = %s %s_{%s} = %s %s_{%s} = %s \\)",
-                  OneMeanHypInfo()$critSign,
-                  testStat,
-                  OneMeanHypInfo()$critAlph,
-                  OneMeanHypInfo()$critSign,
-                  testStat,
-                  OneMeanHypInfo()$alphaVal,
-                  critVal),
-          br(),
-          sprintf("Since the test statistic \\( (%s)\\) falls within the %s region, %s \\( H_{0}\\).",
-                  testStat,
-                  region,
-                  reject)
-          
-        )
-      ),
-      
-      plotOutput('oneMeanHTPlot', width = "75%", height = "300px"),
-      br(),
-      
-      withMathJax(
-        p(tags$b("Conclusion:")),
-        p(
-          sprintf("At the %1.0f%% significance level, the data %s sufficient evidence to reject the null hypothesis \\( (H_{0}) \\) that the population 
-                              mean \\( (\\mu) \\) \\( %s %g\\).",
-                  SigLvl()*100,
-                  suffEvidence,
-                  intrpInfo$nullHyp,
-                  input$hypMean),
-          br(),
-        )
-      )
-    )
   })
   
   
