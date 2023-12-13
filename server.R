ea6594f21e1fa1f04bc760193ebe2284768b089a
Michael Myer <m-myer@live.com>
2023-12-13 01:23:52 -0700

    SLR: updated reg coeff interpretation

--- server.R
+++ server.R
@@ -7989,27 +7989,6 @@ server <- function(session, input, output) {
         output$scatterplot <- renderPlot({ # scatterplot ----
           
           RenderScatterplot(df, input$slrScatterTitle, input$slrScatterXlab, input$slrScatterYlab, input$slrRegLineColour, input$slrScatterColour)
-          # ggplot(df, aes(x = x, y = y)) + 
-          #   geom_point(shape = 19,
-          #              size = 3,
-          #              colour = input$slrScatterColour) + 
-          #   geom_smooth(formula = y ~ x, method = lm,  
-          #               se = FALSE,    
-          #               fullrange = TRUE,
-          #               size = 1,
-          #               colour = input$slrRegLineColour) +
-          #   labs(title = input$slrScatterTitle,
-          #        x = input$slrScatterXlab,
-          #        y = input$slrScatterYlab) +
-          #   theme(plot.title = element_text(size = 24, face = "bold", hjust = 0.5),
-          #         axis.title.x = element_text(size = 16, face = "bold", vjust = -1.5),
-          #         axis.title.y = element_text(size = 16, face = "bold"),
-          #         axis.text.x.bottom = element_text(size = 12),
-          #         axis.text.y.left = element_text(size = 12),
-          #         panel.background = element_rect(fill = "white", colour = "black"),
-          #         panel.grid.major = element_line(colour = "grey90"),
-          #         panel.grid.minor = element_line(colour = "grey90"))
-          
           
         })
           
@@ -8085,13 +8064,21 @@ server <- function(session, input, output) {
             br(),
             br(),
             br(),
-            p(tags$b("Interpretation of regression coefficients:")),
-            sprintf("Within the scope of observation, \\(%s\\) is the estimated value of \\(y\\)
-                    when \\(x\\) = 0. A slope of \\(%s\\) represents the estimated %s in \\(y\\) for a 
-                    unit increase of \\(x\\).",
-                    interceptEstimate,
-                    slopeEstimate,
-                    slopeDirection),
+            p(tags$b("Interpretation of regression coefficients:"),
+              br(),
+              br(),
+              "Within the scope of observation, ", interceptEstimate, " is the estimated value of ", 
+                    em("y"), " when ", em("x"), "= 0. A slope of ", slopeEstimate, 
+                    " represents the estimated ", slopeDirection, " in ", em("y"), 
+                    " for a unit increase of ", em("x.")),
+            # sprintf("Within the scope of observation, %s is the estimated value of ",
+            #         interceptEstimate),
+            
+            # sprintf("when x = 0. A slope of %s represents the estimated %s in y for a 
+            #         unit increase of x.",
+            #         
+            #         slopeEstimate,
+            #         slopeDirection),
             br(),
             br()
           )
