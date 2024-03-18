c366a1c8baeab315a4992d96753033197fc892f8
Michael Myer <m-myer@live.com>
2024-03-18 15:31:58 -0600

    Vertical flip fix

--- R/RenderQQPlot.R
+++ R/RenderQQPlot.R
@@ -35,7 +35,7 @@ RenderQQPlot <- function(dat, plotColour, plotTitle, plotXlab, plotYlab, gridlin
   
   if(flip == 1) {
     qp <- ggpar(qp,
-                orientation = "vertical")
+                orientation = "horiz")
   }
 
   return(qp)
