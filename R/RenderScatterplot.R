fb3f5c609bedc9c3db4a79b35c68b5aa05f69a47
Michael Myer <m-myer@live.com>
2024-04-12 11:29:37 -0600

    Adjusted inputs for greater customization

--- R/RenderScatterplot.R
+++ R/RenderScatterplot.R
@@ -1,13 +1,13 @@
-RenderScatterplot <- function(df, plotTitle, plotXLab, plotYLab, regLineColour, pointColour){
+RenderScatterplot <- function(df, plotTitle, plotXLab, plotYLab, regLineColour, pointColour, lineWidth = 1, pointSize = 3, gridlines){
   
   sPlot <- ggplot(df, aes(x = x, y = y)) + 
     geom_point(shape = 19,
-               size = 3,
+               size = pointSize,
                colour = pointColour) + 
     geom_smooth(formula = y ~ x, method = lm,  
                 se = FALSE,    
                 fullrange = TRUE,
-                linewidth = 1,
+                linewidth = lineWidth,
                 colour = regLineColour) +
     labs(title = plotTitle,
          x = plotXLab,
@@ -18,8 +18,16 @@ RenderScatterplot <- function(df, plotTitle, plotXLab, plotYLab, regLineColour, 
           axis.text.x.bottom = element_text(size = 12),
           axis.text.y.left = element_text(size = 12),
           panel.background = element_rect(fill = "white", colour = "black"),
-          panel.grid.major = element_line(colour = "grey90"),
-          panel.grid.minor = element_line(colour = "grey90"))
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank())
+  
+  if("Major" %in% gridlines) {
+    sPlot <- sPlot + theme(panel.grid.major = element_line(colour = "#D9D9D9"))
+  }
+  
+  if("Minor" %in% gridlines) {
+    sPlot <- sPlot + theme(panel.grid.minor = element_line(colour = "#D9D9D9"))
+  }
   
   return(sPlot)
 }
