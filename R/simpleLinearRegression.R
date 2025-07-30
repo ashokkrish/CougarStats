ef56ff292cd947ac2a5bd421d7733fc7c410a007
Darren Law <darrenlyl@gmail.com>
2025-07-30 01:43:22 -0600

    Implemented diagnostic plots for SLR. Also fixed LR 8 hour warning.

--- R/simpleLinearRegression.R
+++ R/simpleLinearRegression.R
@@ -33,6 +33,7 @@ library(thematic)
 library(tinytex)
 library(writexl)
 library(xtable)
+library(ggResidpanel)
 
 source("R/RenderBoxplot.R")
 source("R/RenderMeanPlot.R")
@@ -107,6 +108,15 @@ SLRMainPanelUI <- function(id) {
               br()
             )
           ), # Scatterplot tabpanel
+          
+          #### ---------------- Diagnostic Plots Tab ---------------------------------
+          tabPanel(
+            title = "Diagnostic Plots",
+            value = "Diagnostic Plots",
+            fluidPage(
+              plotOutput(ns("slrResidualsPanelPlot"))
+            )
+          ), # Diagnostic Plots tabpanel
           
           #### ---------------- Correlation Coefficient Analysis Tab -------------------
           tabPanel(
@@ -554,6 +564,10 @@ SLRServer <- function(id) {
                          ui = FALSE)
           }
         )
+        
+        output$slrResidualsPanelPlot <- renderPlot({
+          resid_panel(model)
+        })
         
         if (summary(model)$coefficients["datx", "Estimate"] > 0) {
           slopeDirection <- "increase"
