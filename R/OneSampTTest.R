79aac72d14a5d70cbd5c98a7ad33031a93fed1b2
ashokkrish <ashokkrish@gmail.com>
2026-03-06 16:45:27 -0700

    Minor changes

--- R/OneSampTTest.R
+++ R/OneSampTTest.R
@@ -28,18 +28,18 @@ TTest <- function(n, xbar, s, mu = 0, alternative = c("two.sided", "less", "grea
 
 # Example usage:
 
-TTest(25, 73, 16, 70, "greater", 0.05)
-
-TTest(18, 237.10, 11.28, 240, "less", 0.05)
-
-TTest(16, 1600, 700, 2000, "less", 0.10)
-
-TTest(8, 160.6, 50, 150, "greater", 0.05)
-
-TTest(48, 21, 11, 30, "less", 0.05)
-
-TTest(9, 6.5, 0.6, 6, "greater", 0.05)
-
-TTest(15, 45, 5, 40, "greater", 0.05)
-
-TTest(30, 2.481, 1.616, 3, "two.sided", 0.10)
+# TTest(25, 73, 16, 70, "greater", 0.05)
+# 
+# TTest(18, 237.10, 11.28, 240, "less", 0.05)
+# 
+# TTest(16, 1600, 700, 2000, "less", 0.10)
+# 
+# TTest(8, 160.6, 50, 150, "greater", 0.05)
+# 
+# TTest(48, 21, 11, 30, "less", 0.05)
+# 
+# TTest(9, 6.5, 0.6, 6, "greater", 0.05)
+# 
+# TTest(15, 45, 5, 40, "greater", 0.05)
+# 
+# TTest(30, 2.481, 1.616, 3, "two.sided", 0.10)
