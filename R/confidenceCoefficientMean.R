788ce89d2e045abfd6d540386364b282d2f76e94
ashokkrish <ashokkrish@gmail.com>
2026-07-24 15:56:53 -0600

    Minor code changes by AK on Jul 24

--- R/confidenceCoefficientMean.R
+++ R/confidenceCoefficientMean.R
@@ -6,13 +6,6 @@ confidence_coefficient_mean <- function(n,
     margin.error <- width / 2
   }
   
-  # Input checks
-  if (n <= 0)
-    stop("'n' must be positive.")
-  if (sigma <= 0)
-    stop("'sigma' must be positive.")
-  if (margin.error <= 0)
-    stop("'margin.error' must be positive.")
   z <- margin.error * sqrt(n) / sigma
   
   # Confidence coefficient
@@ -21,5 +14,5 @@ confidence_coefficient_mean <- function(n,
   return(conf.level)
 }
 
-# Example: fixed n = 100, sigma = 15, want margin error = 2
+# #Example 1:
 #confidence_coefficient_mean(n = 21, sigma = 11, margin.error = 2)
